b195d58fa7e36d06b59ab72d7144377bb3b66556
Ameni Alsaydi <amenialsaydi@pursuit.org>
2019-12-08 20:59:49 -0500

    Changing the stepper value up or down displays the comic from the next or previous day. stepper updates accordingly whenever the comic changes.

--- Images-Lab/Images-Lab/Comic/ComicViewController.swift
+++ Images-Lab/Images-Lab/Comic/ComicViewController.swift
@@ -12,13 +12,29 @@ class ComicViewController: UIViewController {
     
     @IBOutlet weak var comicImage: UIImageView!
     @IBOutlet weak var comicTitle: UILabel!
+    @IBOutlet weak var stepperControl: UIStepper!
     
-    var comicNumber = 2238
+    var comicNumber = 2238 {
+        didSet {
+            loadComic(for: comicNumber)
+            stepperControl.value = Double(comicNumber)
+            
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadComic(for: comicNumber)
+        configureStepper()
     }
+    
+    func configureStepper() {
+        stepperControl.minimumValue = 1.0
+        stepperControl.maximumValue = 2238.0
+        stepperControl.stepValue = 1.0
+        stepperControl.value = Double(comicNumber)
+    }
+    
     
     func loadComic(for comicNumber: Int){
         
@@ -56,18 +72,21 @@ class ComicViewController: UIViewController {
     
     @IBAction func randomButtonPressed(_ sender: UIButton) {
         // The "Random" button should go to a random comic.
-        let randomNumber = Int.random(in: 1...2238)
-        
-        loadComic(for: randomNumber)
-  
+        comicNumber = Int.random(in: 1...2238)
+        // loadComic(for: randomNumber)
     }
-    
     
     @IBAction func recentButtonPressed(_ sender: UIButton) {
         // The "Most recent" button should go to the most recent comic.
-        loadComic(for: comicNumber)
+        comicNumber = 2238
+    }
+    
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+        // Changing the stepper value up or down should display the comic from the next or previous day.
+        comicNumber = Int(sender.value)
         
     }
+    
     
     
 }
